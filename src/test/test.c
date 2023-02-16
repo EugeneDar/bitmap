690f3dc921eacfc79ce97c1b0f163fa3e4905cca
Eugene Dar <evgenedar@mail.ru>
2023-02-16 17:33:07 +0300

    more tests added

--- src/test/test.c
+++ src/test/test.c
@@ -1,25 +1,40 @@
-#include <assert.h>
-#include "../../src/lib/bitmap.h"
 #include "test.h"
 
-void some_test() {
-    struct bitmap_t* bm1 = bm_new();
-    struct bitmap_t* bm2 = bm_new();
+#include <assert.h>
 
-    bm_set_bit(bm1, 1); // 0b00010
-    bm_set_bit(bm2, 4); // 0b10000
+#include "../../src/lib/bitmap.h"
+
+void some_test() {
+    struct bitmap_t *bm1 = bm_new();
+    struct bitmap_t *bm2 = bm_new();
+
+    bm_set_bit(bm1, 1);  // 0b00010
+    bm_set_bit(bm2, 4);  // 0b10000
 
     assert(!bm_eq(bm1, bm2));
 
-    bm_or(bm1, bm2); // bm1 = 0b00010 | 0b10000 = 0b10010
-    bm_clear_bit(bm1, 1); // bm1 = 0b10000
+    bm_or(bm1, bm2);       // bm1 = 0b00010 | 0b10000 = 0b10010
+    bm_clear_bit(bm1, 1);  // bm1 = 0b10000
 
     assert(bm_eq(bm1, bm2));
 }
 
-void set_bit_test () {
-    struct bitmap_t* bm1 = bm_new();
-    struct bitmap_t* bm2 = bm_new();
+void init_test() {
+    struct bitmap_t *bm1 = bm_new();
+    struct bitmap_t *bm2 = bm_new();
+
+    bm_set_bit(bm1, 1);
+    bm_set_bit(bm1, 3);
+    
+    assert(!bm_eq(bm1, bm2));
+
+    bm_init(bm1);
+    assert(bm_eq(bm1, bm2));
+}
+
+void set_bit_test() {
+    struct bitmap_t *bm1 = bm_new();
+    struct bitmap_t *bm2 = bm_new();
 
     assert(bm_eq(bm1, bm2));
 
@@ -40,8 +55,8 @@ void set_bit_test () {
 }
 
 void is_empty_test() {
-    struct bitmap_t* bm1 = bm_new();
-    struct bitmap_t* bm2 = bm_new();
+    struct bitmap_t *bm1 = bm_new();
+    struct bitmap_t *bm2 = bm_new();
 
     assert(bm_is_empty(bm1));
     assert(bm_is_empty(bm2));
@@ -59,14 +74,74 @@ void is_empty_test() {
     assert(bm_is_empty(bm2));
 }
 
+void clear_bit_test() {
+    struct bitmap_t *bm1 = bm_new();
+    struct bitmap_t *bm2 = bm_new();
 
+    assert(bm_eq(bm1, bm2));
 
-// todo clear_bit_test
+    bm_clear_bit(bm1, 1000);
+    assert(bm_eq(bm1, bm2));
+
+    bm_set_bit(bm1, 1000);
+    assert(!bm_eq(bm1, bm2));
+
+    bm_clear_bit(bm1, 1000);
+    assert(bm_eq(bm1, bm2));
+}
+
+void or_test() {
+    struct bitmap_t *bm1 = bm_new();
+    struct bitmap_t *bm2 = bm_new();
+
+    bm_set_bit(bm1, 1);
+    bm_set_bit(bm2, 3);
+    bm_or(bm1, bm2);
+    assert(!bm_eq(bm1, bm2));
+
+    bm_or(bm2, bm1);
+    assert(bm_eq(bm1, bm2));
+}
+
+void and_test() {
+    struct bitmap_t *bm1 = bm_new();
+    struct bitmap_t *bm2 = bm_new();
+
+    bm_set_bit(bm1, 1);
+    bm_set_bit(bm1, 3);
+    bm_set_bit(bm2, 2);
+
+    bm_and(bm1, bm2);
+    assert(!bm_eq(bm1, bm2));
+
+    bm_clear_bit(bm2, 2);
+    assert(bm_eq(bm1, bm2));
+}
+
+void is_disjoint_test() {
+    struct bitmap_t *bm1 = bm_new();
+    struct bitmap_t *bm2 = bm_new();
+
+    bm_set_bit(bm1, 1);
+    bm_set_bit(bm1, 3);
+    bm_set_bit(bm2, 2);
+
+    assert(is_disjoint(bm1, bm2));
+
+    bm_set_bit(bm2, 3);
+
+    assert(!is_disjoint(bm1, bm2));
+}
 
 void run_all_tests() {
     some_test();
+    init_test();
     is_empty_test();
     set_bit_test();
+    clear_bit_test();
+    or_test();
+    and_test();
+    is_disjoint_test();
 }
 
 int main() {
