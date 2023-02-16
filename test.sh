8693f5676f0702ce611c7d2871f41bedbf4ea621
Eugene Dar <evgenedar@mail.ru>
2023-02-16 18:34:45 +0300

    script fixed

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-gcc ./src/test/test.h ./src/test/test.c ./src/lib/bitmap.h ./src/lib/bitmap.c
+gcc ./src/test/test.h ./src/test/test.c ./src/lib/bitmap.h ./src/lib/bitmap.c -Wall
 ./a.out
+rm ./a.out
